c906be5cbc4e16fc95a9433809ac7e1d9b426b61
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-07-14 11:11:53 +0300

    change classname

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -41,7 +41,7 @@ const ButtonIconSVG = {
 
         return (
             <svg
-                class="sb-soundplayer-widget-button-icon"
+                class="sb-soundplayer-button-icon"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
                 fill="currentColor"
